42a7c8289df9492c4e60a3be5a5f338adcb3005d
Titho93 <titho10.93@gmail.com>
2023-09-21 14:18:07 +0200

    [init]file html-css-js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+1. Creare un array con all'interno più oggetti
+2. per ogni oggetto specificare il valore
+3. ciclare tramite ciclo for in ogni membro
+4. in console.log mostrare le informazioni di ogni singolo membro
+5. stampo in html cioò che mi ha mostrato il console.log, con un template literal
